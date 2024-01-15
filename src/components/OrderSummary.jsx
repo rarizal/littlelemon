02574b980d551370aa916ec44f8e446236f141a6
Rizal <rizalisme007@gmail.com>
2024-01-16 00:16:35 +0700

    All Features Complete

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import arrow from "../assets/images/right-arrow.png";
 import "react-datepicker/dist/react-datepicker.css";
@@ -6,6 +6,20 @@ import { useOrder } from "../contexts/OrderContext";
 
 export default function OrderSummary() {
 	const { order, setOrder, printOrder } = useOrder();
+
+	const [bookX, setBookX] = useState(["01/01/2024-18:00"]);
+
+	const [isFalse, setIsFalse] = useState(false);
+
+	function isAvailable(time) {
+		const check = bookX.some((el) => el === `${order.date}-${time}`);
+
+		if (check) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	return (
 		<div className="sum-and-cont">
@@ -21,12 +35,10 @@ export default function OrderSummary() {
 							<DatePicker
 								className="options"
 								selected={new Date(order.date)}
-								// onChange={(date) => setOrder(...date, date)}
 								onChange={(date) => {
 									const formattedDate =
 										date.toLocaleDateString("en");
-									// console.log(new Date(formattedDate));
-									console.log("Variable only", formattedDate);
+									// console.log("Variable only", formattedDate);
 									setOrder({
 										...order,
 										date: formattedDate.toString(),
@@ -36,22 +48,58 @@ export default function OrderSummary() {
 						</div>
 					</div>
 					<div>
-						<div className="order-sum-title">Time </div>
+						<div
+							className="order-sum-title"
+							onClick={() => isAvailable("18:00")}
+						>
+							Time{" "}
+						</div>
 						<select
 							className="options"
-							onChange={(e) =>
+							onChange={(e) => {
 								setOrder({
 									...order,
 									time: e.target.value.toString(),
-								})
-							}
+								});
+								setIsFalse(!isFalse);
+							}}
 						>
-							<option value="17:00">17:00</option>
-							<option value="18:00">18:00</option>
-							<option value="19:00">19:00</option>
-							<option value="20:00">20:00</option>
-							<option value="21:00">21:00</option>
-							<option value="22:00">22:00</option>
+							<option
+								value="17:00"
+								disabled={isAvailable("17:00")}
+							>
+								17:00
+							</option>
+							<option
+								value="18:00"
+								disabled={isAvailable("18:00")}
+							>
+								18:00
+							</option>
+							<option
+								value="19:00"
+								disabled={isAvailable("19:00")}
+							>
+								19:00
+							</option>
+							<option
+								value="20:00"
+								disabled={isAvailable("20:00")}
+							>
+								20:00
+							</option>
+							<option
+								value="21:00"
+								disabled={isAvailable("21:00")}
+							>
+								21:00
+							</option>
+							<option
+								value="22:00"
+								disabled={isAvailable("22:00")}
+							>
+								22:00
+							</option>
 						</select>
 					</div>
 					<div>
@@ -75,7 +123,14 @@ export default function OrderSummary() {
 						</select>
 					</div>
 					<div>
-						<div className="order-sum-title">Occasion</div>
+						<div
+							className="order-sum-title"
+							onClick={() => {
+								console.log(bookX);
+							}}
+						>
+							Occasion
+						</div>
 						<select
 							className="options"
 							onChange={(e) =>
@@ -92,6 +147,7 @@ export default function OrderSummary() {
 				className="cont-button"
 				onClick={() => {
 					printOrder();
+					setBookX([...bookX, `${order.date}-${order.time}`]);
 				}}
 			>
 				<img src={arrow} alt="Arrow Button" width="100"></img>
